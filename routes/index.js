302e31d767db29ee5c87923fd5dab8a7e6537a15
Mikhail Korchevskiy <mkorchevskiy@ryerson.ca>
2018-07-28 15:41:46 -0400

    added logo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: __dirname });
+  res.render('index', { logo_path: path.join('http://localhost:3000/', 'views/images/logo.png') });
 });
 
 router.get('/html', (req, res) => {
