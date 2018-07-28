f4d32a764712bc5d0fa1c31f98e5a77451a2091e
Mikhail Korchevskiy <mkorchevskiy@ryerson.ca>
2018-07-28 13:38:53 -0400

    added Bulma UI

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+const path = require('path');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: __dirname });
+});
+
+router.get('/html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
